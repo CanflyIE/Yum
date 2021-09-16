859042ec48212c7f05a9f0fc1713acd3b3bed1ef
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-09-16 09:54:10 -0400

    MainPleine first commit #5

---
+++ src/Fonctions/MainPleine.java
@@ -0,0 +1,12 @@
+package Fonctions;
+
+public class MainPleine {
+
+    // Procédure qui vérifie si vous avez une main pleine
+    public static void main(String[] args){
+        int[] array = {2, 5, 6, 1, 8};
+
+        
+    }
+
+}
