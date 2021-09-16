22c7d1a038f9586d4e09a7410b7d867aa8f76cdd
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-09-16 09:59:21 -0400

    MainPleine #5

--- src/Fonctions/MainPleine.java
+++ src/Fonctions/MainPleine.java
@@ -2,11 +2,17 @@ package Fonctions;
 
 public class MainPleine {
 
-    // Procédure qui vérifie si vous avez une main pleine
     public static void main(String[] args){
+
         int[] array = {2, 5, 6, 1, 8};
 
-        
+
+    }
+
+    // Procédure qui vérifie si vous avez une main pleine
+    public boolean checkMainPleine(int[] array) {
+
+        return false;
     }
 
 }
