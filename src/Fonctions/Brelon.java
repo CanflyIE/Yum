5e0059b0d1e1cfa4d82439c151d2195d5b6b38af
IlyesEssid <61743050+CanflyIE@users.noreply.github.com>
2021-09-20 15:33:49 +0200

    Brelon

---
+++ src/Fonctions/Brelon.java
@@ -0,0 +1,31 @@
+package Fonctions;
+
+public class Brelon {
+
+
+    public static void main(String[] args) {
+        int[] tab = {1,2,2,2,5};
+        System.out.println(verifBrelon(tab));
+    }
+
+    public static int verifBrelon(int[] tab){
+
+        int total = 0;
+
+        for (int i: tab) {
+            int nombreDeDoublon = 0;
+            for ( int y : tab ) {
+                if (i==y){
+                    nombreDeDoublon++;
+                }
+
+            }
+            if(nombreDeDoublon >= 3){
+                total = i*3;
+                break;
+            }
+        }
+            return total;
+    }
+
+    }
