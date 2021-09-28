3da350ca82d8d3b8e1dce491a203ffa8ac3ec7a8
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-09-28 10:24:06 -0400

    Tests fonctions utilitaires à la boucle de jeu #9

--- src/YumVsEtudTest.java
+++ src/YumVsEtudTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.*;
 
+import java.util.Arrays;
+
 public class YumVsEtudTest {
 
     @Test
@@ -23,5 +25,44 @@ public class YumVsEtudTest {
         Assertions.assertFalse(YumVsEtud.isMainPleine(tab2));
     }
 
+    @Test
+    public void test_tableADoublons() {
+        int[] tab1 = {1, 2, 4, 3, 4};
+        int[] tab2 = {1, 2, 4, 3, 5};
+        Assertions.assertTrue(YumVsEtud.tableADoublons(tab1));
+        Assertions.assertFalse(YumVsEtud.tableADoublons(tab2));
+    }
+
+    @Test
+    public void test_initTableDes() {
+        int[] tab = new int[5];
+        YumVsEtud.initTableDes(tab);
+        Assertions.assertTrue(tab[0] != 0);
+        Assertions.assertTrue(tab[1] != 0);
+        Assertions.assertTrue(tab[2] != 0);
+        Assertions.assertTrue(tab[3] != 0);
+        Assertions.assertTrue(tab[4] != 0);
+    }
+
+    @Test
+    public void test_PossedeValInvalide() {
+        int[] tab1 = {1, 5, 3, 4, 6};
+        int[] tab2 = {1, 2, 3, 4, 5};
+        Assertions.assertTrue(YumVsEtud.tablePossedeValInvalide(tab1));
+        Assertions.assertFalse(YumVsEtud.tablePossedeValInvalide(tab2));
+    }
+
+    @Test
+    public void test_relancerDesChoisis() {
+        int[] tab = {1, 3, 5, 4, 6};
+        Assertions.assertFalse(Arrays.equals(YumVsEtud.relancerDesChoisis(tab, new int[]{1, 5}), new int[]{1, 3, 5, 4, 6}));
+    }
+
+    @Test
+    public void test_intToArray() {
+        int integer = 12345;
+        Assertions.assertArrayEquals(YumVsEtud.intToArray(integer), new int[]{5, 4, 3, 2, 1});
+        Assertions.assertFalse(Arrays.equals(YumVsEtud.intToArray(integer), new int[]{5, 4, 3, 2, 2}));
+    }
 
 }
